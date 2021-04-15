b65dd30382d8ad87331c8ab83e9c88f96556d13d
memdrive <82234952+memdrive@users.noreply.github.com>
2021-04-14 17:28:29 -0700

    main branch and gh-pages branch

---
+++ src/components/Listening.js
@@ -0,0 +1,21 @@
+
+function Listening() {
+    return (
+        <div class="body-container">
+            <div class="post">
+                <h2>SPIRIT OF THE BEEHIVE <sup><a href="https://www.spiritofthebeehiveband.com">1</a></sup> and Milly <sup><a href="https://www.millymusicgroup.com/">2</a></sup> release, &#39;ENTERNTAINMENT, DEATH&#39; and &#39;Wish Goes On&#39;</h2>
+                    <p id="timestamp">April 9<sup>th</sup>, 2021</p>
+                    <p>I really wanted this to be a working SPA React app with, what I was planning to use, the &quot;Universal-Router&quot; router instead of React&#39;s router React Router, because I could save some space, I guess. Aside from that, I decided to focus more on content than functionality; for now this will just be a static page dedicated to the release of &#39;ENTERNTAINMENT, DEATH&#39; and &#39;Wish Goes On&#39;.</p>
+                    <p>Today, SPIRIT OF THE BEEHIVE and Milly release, an album &#39;ENTERNTAINMENT, DEATH&#39; and EP &#39;Wish Goes On&#39; respectively. &#39;ED&#39; is 11 tracks long (3 being previously released singles; 2<sup>nd</sup> track &quot;THERE&#39;S NOTHING YOU CAN&#39;T DO&quot;, 7<sup>th</sup> track &quot;THE SERVER IS IMMERSED&quot;, and 10<sup>th</sup> track &quot;I SUCK THE DEVIL&#39;S COCK&quot;. &#39;WGO&#39; is 5 tracks long (3 also being previously released singles; 2<sup>nd</sup> track &quot;Denial&quot;, 3<sup>rd</sup> track &quot;Star Thistle Blossom&quot;, and 5<sup>th</sup> track &quot;Birds Fly Free&quot;. They&#39;re being grouped together out of convenience; both make alternative indie rock lo-fi more dream pop than shoegaze esque music, they will inevitably release music together, and I listen to both of them. </p>
+                    <p>I haven&#39;t listened to either project in its entirety, excluding the previously released singles, but today I get the opportunity to listen to both the same day they are released. I will first listen to &#39;ED&#39; in it&#39;s entirety, then &#39;WGO&#39;, and give my take aways for both projects. I listened to both, they were amazing thank you.</p>
+                    
+                    <p id="used-links">Links:</p>
+
+                    <p>[1] : <a href="https://www.spiritofthebeehiveband.com">https://spiritofthebeehiveband.com</a> <br></br>
+                       [2] : <a href="https://www.millymusicgroup.com">https://www.millymusicgroup.com</a></p>
+            </div>
+        </div>
+    )
+}
+
+export default Listening;
