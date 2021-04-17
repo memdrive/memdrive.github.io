7527e6d16901f31591b666d2dd48d37dca528e8a
memdrive <82234952+memdrive@users.noreply.github.com>
2021-04-16 23:34:15 -0700

    jquery to .load() html to home index

---
+++ action.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    // will be generated dynamically
+    $("#listening-preview").load("listening/spiritoftheb-milly-album-release.html h2",
+    function() {
+        $("#listening-preview").attr("href", "listening/spiritoftheb-milly-album-release.html")
+    }
+    );
+
+
+}
