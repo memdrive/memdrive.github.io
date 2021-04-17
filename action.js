6ca68136d406e66c9dd1cd59cacbfe3701c57c97
memdrive <82234952+memdrive@users.noreply.github.com>
2021-04-17 01:34:26 -0700

    horsebeach2014

--- action.js
+++ action.js
@@ -1,10 +1,15 @@
 window.onload = function() {
     // will be generated dynamically
-    $("#listening-preview").load("listening/spiritoftheb-milly-album-release.html h2",
+    $("#listening-preview-one").load("listening/spiritoftheb-milly-album-release.html h2",
     function() {
-        $("#listening-preview").attr("href", "listening/spiritoftheb-milly-album-release.html")
+        $("#listening-preview-one").attr("href", "listening/spiritoftheb-milly-album-release.html")
     }
     );
+
+    $("#listening-preview-two").load("listening/horsebeach2014.html h2",
+    function() {
+        $("#listening-preview-two").attr("href", "listening/horsebeach2014.html")
+    });
 
 
 }
